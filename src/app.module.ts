aed403a7858de9b13dbfb1f32c9d7dc4afddf21e
Shimpali Deshpande <shimpali.deshpande@gmail.com>
2023-02-18 17:31:39 +0530

    Added the ristg module adn TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
+import { DatabaseModule } from './database/database.module';
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/posts/post.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Column, CreateDateColumn, Entity, Index, JoinTable,
+  ManyToMany,
+  ManyToOne, OneToMany, PrimaryGeneratedColumn, RelationId
+} from 'typeorm';
+// import Category from '../categories/category.entity';
+// import Comment from '../comments/comment.entity';
+// import User from '../users/user.entity';
+
+@Entity()
+class Post {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  public title: string;
+
+  @Column()
+  public content: string;
+
+  // @Column('text', { array: true })
+  // public paragraphs: string[];
+
+  // @Index('post_authorId_index')
+  // @ManyToOne(() => User, (author: User) => author.posts)
+  // public author: User;
+
+  // @RelationId((post: Post) => post.author)
+  // public authorId: number;
+
+  // @ManyToMany(() => Category, (category: Category) => category.posts)
+  // @JoinTable()
+  // public categories: Category[];
+
+  // @OneToMany(() => Comment, (comment: Comment) => comment.post)
+  // public comments: Comment[];
+
+  // @CreateDateColumn({ type: 'timestamp' })
+  // createdAt: Date;
+
+  // @Column({
+  //   type: 'timestamp',
+  //   nullable: true,
+  // })
+  // scheduledDate?: Date;
+}
+
+export default Post;
